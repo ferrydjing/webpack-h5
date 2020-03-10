ef099bed6fefa198977732411738b01c57b71dfe
dan ferry <danferry@dandeMacBook-Pro.local>
2020-03-10 13:06:44 +0800

    update 添加html-loader

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import './sass/global.scss'
 import '@babel/polyfill'
 import fp from 'lodash/fp'
-
+import icon from './assets/imgs/no-data.png'
 import { b } from './js/hello'
 
 console.log(b, fp.join(',', 'bbb'))
+
+// let aa = document.createElement('img')
+// aa.src = icon
+// document.body.appendChild(aa)
+
 const a = 0
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const uglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin')
+const opimizeCss = require('optimize-css-assets-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: '[name].[hash:8].js',
+    filename: 'js/[name].[hash:8].js',
     // chunkFilename: '[name]-vendors-[hash:8].js',
     path: path.resolve(__dirname, 'dist')
   },
@@ -21,8 +23,21 @@ module.exports = {
           options: {
             presets: ['@babel/preset-env'],
             plugins: ['@babel/plugin-transform-runtime']
+            // outputPath: 'js/'
           }
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src'],
+              publicPath: './'
+            }
+          }
+        ]
       },
       {
         test: /\.scss$/,
@@ -30,21 +45,46 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: path.resolve(__dirname, 'dist')
+              // publicPath: path.resolve(__dirname, 'dist')
+              outputPath: 'css'
             }
           },
           'css-loader',
-          'sass-loader'
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: loader => [
+                // 可以配置多个插件
+                require('autoprefixer')({
+                  overrideBrowserslist: [
+                    '> 1%',
+                    'last 2 versions',
+                    'not ie <= 8'
+                  ]
+                })
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
         ]
       },
       {
         test: /\.(png|git|jpg|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
             options: {
-              limit: 3000,
-              name: '[name].[hash:8].[ext]'
+              esModule: false,
+              limit: 1024,
+              name: 'img/[name].[hash:8].[ext]',
+              publicPath: '../'
             }
           }
         ]
@@ -66,9 +106,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name].[hash:8].css',
-      chunkFilename: '[id].css'
-    })
+      filename: 'css/[name].[hash:8].css',
+      chunkFilename: 'css/[id].css'
+    }),
+    require('autoprefixer')
   ],
   resolve: {
     extensions: ['.js', '.json', '.jsx', '.scss', '.css'], //用到文件的扩展名
@@ -80,7 +121,34 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all'
-    }
+    },
+    minimizer: [
+      new uglifyJsWebpackPlugin({
+        cache: true, //是否缓存
+        parallel: true, //是否并发打包，同时打包多个文件
+        sourceMap: true, //打包后的代码与源码的映射，方便调试
+        uglifyOptions: {
+          // 在UglifyJs删除没有用到的代码时不输出警告
+          warnings: false,
+          output: {
+            // 删除所有的注释
+            comments: false,
+            // 最紧凑的输出
+            beautify: false
+          },
+          compress: {
+            // 删除所有的 `console` 语句
+            // 还可以兼容ie浏览器
+            drop_console: true,
+            // 内嵌定义了但是只用到一次的变量
+            collapse_vars: true,
+            // 提取出出现多次但是没有定义成变量去引用的静态值
+            reduce_vars: true
+          }
+        }
+      }),
+      new opimizeCss()
+    ]
   },
   devServer: {
     port: 3000,
